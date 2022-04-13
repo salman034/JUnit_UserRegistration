8b0ecfa76ead0076dfea2f9053cc05b3f808db97
Salman Pasha <85282497+salman034@users.noreply.github.com>
2022-04-13 06:19:30 +0530

    Update JUnitTest.java

--- src/main/JUnitTest.java
+++ src/main/JUnitTest.java
@@ -2,6 +2,6 @@ package main;
 
 public class JUnitTest {
     public static void main(String[] args) {
-        System.out.println("Welcome To The JUnit Test Problems!");
+        System.out.println("Welcome To The JUnit Testing Problems!");
     }
 }
