3987d350815d0130d7937d2aa9857580986a63b4
unknown <you@example.com>
2022-04-17 12:09:47 +0530

    Added: Parameterised Test to validate multiple entry for the Email Address

--- src/com/bl/junitTesting/UserRegistration.java
+++ src/com/bl/junitTesting/UserRegistration.java
@@ -3,33 +3,13 @@ package com.bl.junitTesting;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9+_.-]+[@]{1}[a-zA-Z0-9]+[.a-z]{4}([.a-zA-Z]{3})*$";
-    private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-zA-Z]{3,15}$";
-    private static final String LAST_NAME_PATTERN = "^[A-Z]{1}[a-zA-Z]{3,15}$";
-    private static final String MOBILE_NO_PATTERN = "^[0-9]{2}[ ][0-9]{10}$";
-    private static final String PASSWORD_PATTERN = "([!@#$%^&*()]?[A-Za-z0-9]+){8,}$";
+    public static final String EMAIL_PATTERN = "^[a-zA-Z0-9+_.-]+[@][a-zA-Z0-9]+[.a-z]{3,4}([.a-zA-Z,]{3})*$";
 
-    public boolean firstName(String firstName) {
-        boolean mathPattern = Pattern.matches(FIRST_NAME_PATTERN, firstName);
-        return mathPattern;
-    }
-
-    public boolean lastName(String lastName) {
-        boolean mathPattern = Pattern.matches(LAST_NAME_PATTERN, lastName);
-        return mathPattern;
-    }
-
-    public boolean mobileNumber() {
-        boolean matchesString = Pattern.matches(MOBILE_NO_PATTERN, "91 9845715264");
-        return matchesString;
-    }
-
-    public boolean passwordTest(String password) {
-        boolean matchesString = Pattern.matches(PASSWORD_PATTERN, password);
-        return matchesString;
-    }
-    public boolean gmailTest(String email) {
-        boolean matchesString = Pattern.matches(EMAIL_PATTERN, email);
-        return matchesString;
+    public boolean emailTest(String gmail, boolean value) {
+        boolean matchesCode = Pattern.matches(EMAIL_PATTERN, gmail);
+        if(matchesCode == value)
+            return value;
+        else
+            return false;
     }
 }
--- src/com/bl/junitTesting/UserRegistrationTest.java
+++ src/com/bl/junitTesting/UserRegistrationTest.java
@@ -1,44 +1,60 @@
 package com.bl.junitTesting;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.Test;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(value = Parameterized.class)
 public class UserRegistrationTest {
-    private Assertions Assert;
+    private UserRegistration registration;
+    private String emailPattern;
+    private static boolean expectedResult;
 
-    @Test
-    public void givenFirstNameWhenPropershouldReturnTrue() {
-        UserRegistration userDetails = new UserRegistration();
-        boolean testStatus = userDetails.firstName("Salman");
-        Assert.assertTrue(testStatus);
+    public UserRegistrationTest(String emailPattern, boolean expectedResult) {
+        this.emailPattern = emailPattern;
+        this.expectedResult = expectedResult;
+    }
+
+    @Before
+    public void initialize() {
+        registration = new UserRegistration();
+    }
+
+    @Parameterized.Parameters
+    public static Collection emails() {
+        return Arrays.asList(new Object[][]{
+                        {"abc-100@yahoo.com", true},
+                        {"abc.100@yahoo.com", true},
+                        {"abc111@gmail.com", true},
+                        {"abc-100@abc.com", true},
+                        {"abc-100@abc.net", true},
+                        {"abc.100@abc.com.au", true},
+                        {"abc@gmail.com.com", true},
+                        {"abc@1.com", true},
+                        {"abc+100@gmail.com", true},
+                        {"abc", false},
+                        {".abc+100@gmail.com", false},
+                        {"abc+100@gmail.com", false},
+                        {"abc+100@gmail.a", false},
+                        {"abc+100@.gmail.com", false},
+                        {"abc+100*()@gmail.com", false},
+                        {"abc@abc.com", false}
+                }
+        );
     }
 
     @Test
-    public void givenLastNameWhenPropershouldReturnTrue() {
-        UserRegistration userDetails = new UserRegistration();
-        boolean testStatus = userDetails.lastName("Pasha");
-        Assert.assertTrue(testStatus);
-    }
-
-    @Test
-    public void givenPasswordShouldReturnTrueWhenSpecialCharaterhaveAnyOne() {
-        UserRegistration userDetails = new UserRegistration();
-        boolean testStatus = userDetails.passwordTest("Salman@889");
-        Assert.assertTrue(testStatus);
-    }
-
-    @Test
-    public void givenEmailShouldReturnTrueWhenalltheConditionSatisfied() {
-        UserRegistration userDetails = new UserRegistration();
-        boolean testStatus = userDetails.gmailTest("mkspasha.xyz@gmail.com");
-        Assert.assertTrue(testStatus);
-    }
-
-    @Test
-    public void givenMessageShouldReturnSadWhenTestIsPassed(){
-        MoodAnalyser mood =  new MoodAnalyser();
-        String testStatus = mood.messageContainHappyReturnHappyElseSad("This is Sad message");
-        System.out.println(testStatus);
-        Assert.assertEquals("Sad",testStatus);
+    public void givenEmailAsVariable_ShouldReturnAsPerTheParameterizedResult() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean userTest = userRegistration.emailTest(emailPattern, expectedResult);
+        System.out.println("sum of numbers = : " + userTest);
+        Assert.assertEquals(expectedResult,userTest);
     }
 }
